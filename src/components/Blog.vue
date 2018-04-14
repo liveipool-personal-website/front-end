3a358df2722f42a9a643489308f7f18da82e5ee4
Liveipool <123653079@qq.com>
2018-04-14 19:19:38 +0800

    增加博客页面在小屏幕上访问的显示

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="blog">
-    <el-row class="content-container">
-      <el-col class="left-content" :sm="6" :md="6" :lg="6" :xl="6">
+    <el-row class="big-content-container hidden-xs-only">
+      <el-col
+        class="left-content"
+        :sm="6" :md="6" :lg="6" :xl="6">
         <el-input
-          class="search-bar"
           placeholder="请输入博客名进行搜索"
           prefix-icon="el-icon-search"
           v-model="searchBar">
@@ -12,13 +13,32 @@
         <blogs-info :blogsInfo="blogsInfo"></blogs-info>
       </el-col>
       <el-col
-        @scroll.native="scrollEvent" class="right-content"
+        class="right-content"
+        @scroll.native="scrollEvent"
         :sm="18" :md="18" :lg="18" :xl="18">
         <normal-head-bar :buttons="buttons"></normal-head-bar>
         <floating-head-bar id="floatingHeadBar" :buttons="buttons"></floating-head-bar>
         <blog-content title="如何评价中山大学" :content="content"></blog-content>
       </el-col>
     </el-row>
+
+    <div class="small-content-container hidden-sm-and-up">
+      <el-input
+        class="small-search-bar"
+        placeholder="请输入博客名进行搜索"
+        prefix-icon="el-icon-search"
+        v-model="searchBar">
+      </el-input>
+      <el-dropdown trigger="click" @command="handleCommand">
+        <i class="el-icon-menu"></i>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item command="Home">主页</el-dropdown-item>
+          <el-dropdown-item command="About">关于作者</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+      <categorys :categorys="categorys"></categorys>
+      <blogs-info :blogsInfo="blogsInfo"></blogs-info>
+    </div>
   </div>
 </template>
 
@@ -86,6 +106,7 @@ export default {
     };
   },
   methods: {
+    // 上下滑动博客内容部分的滑动时间
     scrollEvent(event) {
       const floatingHeadBar = document.getElementById('floatingHeadBar');
       if (event.target.scrollTop > this.oldScrollTop || event.target.scrollTop < 58) {
@@ -94,6 +115,10 @@ export default {
         floatingHeadBar.style.display = '';
       }
       this.oldScrollTop = event.target.scrollTop;
+    },
+    // 跳转按钮下拉菜单的监听事件
+    handleCommand(command) {
+      this.$router.push({ name: command });
     },
   },
   mounted() {
@@ -123,7 +148,7 @@ export default {
   height: 100vh;
 }
 
-.content-container {
+.big-content-container {
   width: 100%;
   height: 100%;
 }
@@ -137,12 +162,37 @@ export default {
   height: 100%;
   overflow-y: auto;
 }
+
+.small-content-container {
+  width: 100%;
+  height: 100%;
+  background-color: #fef4eb;
+}
+
+.small-search-bar {
+  width: calc(100% - 40px);
+}
 </style>
 
 <style>
-.blog .content-container .left-content .el-input__inner {
+.blog .big-content-container .left-content .el-input__inner,
+.blog .small-content-container .el-input__inner {
   border: 0;
   background-color: #fef4eb;
   color: #3c3c3c;
+}
+
+.blog .small-content-container .el-dropdown {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 40px;
+  height: 40px;
+}
+.blog .small-content-container .el-dropdown .el-icon-menu {
+  display: inline-block;
+  margin: 5px;
+  cursor: pointer;
+  font-size: 30px
 }
 </style>
